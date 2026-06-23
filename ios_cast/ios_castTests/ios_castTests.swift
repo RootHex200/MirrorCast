13c6c2bab099306f6637de52c26f649a099148f6
Roothex200 <sabiturrahman60@gmail.com>
2026-06-24 01:10:04 +0600

    Add ios_cast/ios_castTests/ios_castTests.swift

---
+++ ios_cast/ios_castTests/ios_castTests.swift
@@ -0,0 +1,19 @@
+//
+//  ios_castTests.swift
+//  ios_castTests
+//
+//  Created by Roothex200 on 6/23/26.
+//
+
+import Testing
+@testable import ios_cast
+
+struct ios_castTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
