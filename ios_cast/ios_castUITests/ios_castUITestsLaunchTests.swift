901063311fc6063f3a01056a052c49cf66d26093
Roothex200 <sabiturrahman60@gmail.com>
2026-06-24 01:10:04 +0600

    Add ios_cast/ios_castUITests/ios_castUITestsLaunchTests.swift

---
+++ ios_cast/ios_castUITests/ios_castUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  ios_castUITestsLaunchTests.swift
+//  ios_castUITests
+//
+//  Created by Roothex200 on 6/23/26.
+//
+
+import XCTest
+
+final class ios_castUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
