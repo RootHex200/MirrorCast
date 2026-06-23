c5e07222f76b3be7d9d91a27c0aa683083a7965c
Roothex200 <sabiturrahman60@gmail.com>
2026-06-24 02:20:08 +0600

    P2: declare kotlinx-coroutines-core as main dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
+    implementation(libs.kotlinx.coroutines.core)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
